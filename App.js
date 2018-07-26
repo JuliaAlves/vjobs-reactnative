680bdaf6615762c7327745b5dd43a1e620243ab3
Júlia Alves de Arruda <julieta.a.arruda@gmail.com>
2018-07-26 12:57:02 -0300

    Lista de vagas e navigation

--- App.js
+++ App.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, View, FlatList } from 'react-native';
-import List from './Components/List/List.js'
+import RootStack from './Components/Navigation/Navigation.js';
+
 export default class App extends React.Component { 
-  
   render() {
-    const vagas= [
-      {nome:'Vaga1', id:'1'},
-      {nome:'Vaga2', id:'2'},
-      {nome:'Vaga3', id:'3'},
-    ]
     return (
-      <SafeAreaView style={styles.container}>
-        <List vagas={vagas}/>
-      </SafeAreaView>
+      <RootStack />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
